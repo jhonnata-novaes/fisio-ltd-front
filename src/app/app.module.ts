d149025f6d9c117d9788162f4c7a95c48af3870d
LeonardoIBraim <andrearia2015@gmail.com>
2024-10-07 13:45:54 -0300

    v1.4.4
    -Faltando apenas funcionar a conexao com o backend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { PacientesComponent } from './pacientes/pacientes.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NovopacienteComponent } from './novopaciente/novopaciente.component';
-import { HttpClientModule } from '@angular/common/http'; // Importação do HttpClientModule
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
@@ -20,6 +21,7 @@ import { HttpClientModule } from '@angular/common/http'; // Importação do Http
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
     NgbModule,
     HttpClientModule  // Adicionei o HttpClientModule aqui
   ],
--- src/app/novopaciente/novopaciente.component.ts
+++ src/app/novopaciente/novopaciente.component.ts
@@ -1,23 +1,24 @@
 import { Component } from '@angular/core';
 import { PacienteService } from '../services/paciente.service';  // Importar o serviço
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 // Definindo uma interface para os dados do paciente
-interface DadosBasicos {
-  nome: string;
-  dataAvaliacao: Date;
-  estadoCivil: string;
-  nacionalidade: string;
-  naturalidade: string;
-  dataNascimento: Date;
-  peso: number;
-  altura: number;
-  endereco: string;
-  numeroIdentidade: string;
-  telefone: string;
-  email: string;
-  profissao: string;
-  diagnosticoClinico?: string;
-}
+// interface DadosBasicos {
+//   nome: string;
+//   dataAvaliacao: Date;
+//   estadoCivil: string;
+//   nacionalidade: string;
+//   naturalidade: string;
+//   dataNascimento: Date;
+//   peso: number;
+//   altura: number;
+//   endereco: string;
+//   numeroIdentidade: string;
+//   telefone: string;
+//   email: string;
+//   profissao: string;
+//   diagnosticoClinico?: string;
+// }
 
 @Component({
   selector: 'app-novopaciente',
@@ -25,60 +26,50 @@ interface DadosBasicos {
   styleUrls: ['./novopaciente.component.scss']
 })
 export class NovopacienteComponent {
-  paciente: DadosBasicos = { // Tipo do paciente definido
-    nome: '',
-    dataAvaliacao: new Date(),
-    estadoCivil: '',
-    nacionalidade: '',
-    naturalidade: '',
-    dataNascimento: new Date(),
-    peso: 0,
-    altura: 0,
-    endereco: '',
-    numeroIdentidade: '',
-    telefone: '',
-    email: '',
-    profissao: '',
-    diagnosticoClinico: ''
-  };
+  
+  form = new FormGroup({
+    nome: new FormControl<string>('', Validators.required),
+    dataAvaliacao: new FormControl<string>('', Validators.required),
+    estadoCivil: new FormControl<string>('', Validators.required),
+    nacionalidade: new FormControl<string>('', Validators.required),
+    naturalidade: new FormControl<string>('', Validators.required),
+    dataNascimento: new FormControl<string>('', Validators.required),
+    peso: new FormControl<string>('', Validators.required),
+    altura: new FormControl<string>('', Validators.required),
+    endereco: new FormControl<string>('', Validators.required),
+    numeroIdentidade: new FormControl<string>('', Validators.required),
+    telefone: new FormControl<string>('', Validators.required),
+    email: new FormControl<string>('', Validators.required),
+    profissao: new FormControl<string>('', Validators.required),
+    diagnosticoClinico: new FormControl<string>('', Validators.required),
+  });
+  // paciente: DadosBasicos = { // Tipo do paciente definido
+  //   nome: '',
+  //   dataAvaliacao: new Date(),
+  //   estadoCivil: '',
+  //   nacionalidade: '',
+  //   naturalidade: '',
+  //   dataNascimento: new Date(),
+  //   peso: 0,
+  //   altura: 0,
+  //   endereco: '',
+  //   numeroIdentidade: '',
+  //   telefone: '',
+  //   email: '',
+  //   profissao: '',
+  //   diagnosticoClinico: ''
+  // };
 
   constructor(private pacienteService: PacienteService) { }
 
-  // Função para verificar se um campo é obrigatório
-  private campoObrigatorio(campo: string): boolean {
-    return campo.trim() === '';
-  }
+  // // Função para verificar se um campo é obrigatório
+  // private campoObrigatorio(campo: string): boolean {
+  //   return campo.trim() === '';
+  // }
 
   // Função para salvar os dados do paciente
-  salvarPaciente() {
-    // Verifica se todos os campos obrigatórios estão preenchidos
-    if (
-      this.campoObrigatorio(this.paciente.nome) || 
-      this.campoObrigatorio(this.paciente.email) || 
-      this.campoObrigatorio(this.paciente.endereco) ||
-      this.campoObrigatorio(this.paciente.estadoCivil) || 
-      this.campoObrigatorio(this.paciente.nacionalidade) || 
-      this.campoObrigatorio(this.paciente.naturalidade) ||
-      this.campoObrigatorio(this.paciente.numeroIdentidade) || 
-      this.campoObrigatorio(this.paciente.profissao) || 
-      this.campoObrigatorio(this.paciente.telefone) || 
-      this.paciente.peso <= 0 || 
-      this.paciente.altura <= 0 ||
-      !this.paciente.dataAvaliacao || 
-      !this.paciente.dataNascimento
-    ) {
-      console.error('Por favor, preencha todos os campos obrigatórios corretamente.');
-      return; // Não envia os dados se algum campo estiver vazio ou inválido
-    }
-
-    // Se todos os campos obrigatórios estiverem preenchidos
-    this.pacienteService.salvarPaciente(this.paciente).subscribe({
-      next: (response) => {
-        console.log('Paciente salvo com sucesso', response);
-      },
-      error: (error) => {
-        console.error('Erro ao salvar o paciente', error);
-      }
-    });
+  
+  salvar(){
+    console.log(this.form.value);
   }
 }
